6ee42780397a4076d289ba58aaa5948dc2c639ee
LiquidPlummer <kyle.plummer@revature.net>
2025-03-19 17:29:51 -0400

    disabling middleware while we test and debug

--- backend/app/main.py
+++ backend/app/main.py
@@ -67,30 +67,33 @@ async def route_guard(request: Request, call_next):
 
     Before the response is sent, execution returns to the middleware, where we make sure the access_token is updated before responding.
     """
-    if request.url.path in UNSECURE_ROUTES:
-        return await call_next(request)
+    print(f'\n\nDEBUG PATH: {request.url.path}\n\n')
 
-    access_token = request.headers.get("Access-Token")
-    if not access_token:
-        return Response(status_code = 400, content = "Missing Access Token")
+    return await call_next()
 
-    print(f'Middleware - Access token: {access_token}')
-    try:
-        if not verify_token_exp(access_token):
-            refresh_response = workos.user_management.authenticate_with_refresh_token(refresh_token=get_refresh_token(access_token))
-            refresh_session(access_token, refresh_response.access_token, refresh_response.refresh_token)
-            access_token = refresh_response.access_token
-        response: Response = await call_next(request)
-        response.headers['Access-Token'] = access_token
-        return response
+    # if request.url.path in UNSECURE_ROUTES:
+    #     return await call_next(request)
 
-    except exceptions.BadRequestException:
-        return Response(status_code = 400, content = "Invalid workos session")
-    except NoMatchingKeyException as e:
-        return Response(status_code = 400, content = "Bad Token Header")
-    except Exception as e:
-        print(e)
-        return Response(status_code = 500, content = "Something went wrong when verifying the access token")
+    # access_token = request.headers.get("Access-Token")
+    # if not access_token:
+    #     return Response(status_code = 400, content = "Missing Access Token")
+
+    # try:
+    #     if not verify_token_exp(access_token):
+    #         refresh_response = workos.user_management.authenticate_with_refresh_token(refresh_token=get_refresh_token(access_token))
+    #         refresh_session(access_token, refresh_response.access_token, refresh_response.refresh_token)
+    #         access_token = refresh_response.access_token
+    #     response: Response = await call_next(request)
+    #     response.headers['Access-Token'] = access_token
+    #     return response
+
+    # except exceptions.BadRequestException:
+    #     return Response(status_code = 400, content = "Invalid workos session")
+    # except NoMatchingKeyException as e:
+    #     return Response(status_code = 400, content = "Bad Token Header")
+    # except Exception as e:
+    #     print(e)
+    #     return Response(status_code = 500, content = "Something went wrong when verifying the access token")
 
 app.include_router(api_router)
 
