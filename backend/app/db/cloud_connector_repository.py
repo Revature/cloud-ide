3e159d6793ed1a8b3d3729406e755467a22de0bf
RevvingUpThatHill <theodore.balashov@revature.com>
2025-03-19 14:21:28 -0400

    import fixes

--- backend/app/db/cloud_connector_repository.py
+++ backend/app/db/cloud_connector_repository.py
@@ -4,5 +4,5 @@ from sqlmodel import Session, select
 
 def find_cloud_connector_by_id(session:Session, id:str) -> CloudConnector:
     """Select a cloud connector by its ID."""
-    statement: Select[CloudConnector] = select(CloudConnector).where(CloudConnector.id == id)
+    statement = select(CloudConnector).where(CloudConnector.id == id)
     return session.exec(statement).first()
