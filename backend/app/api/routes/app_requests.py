311c42ef69f5e7c432476ccd600ffd77b4238e7b
RevvingUpThatHill <theodore.balashov@revature.com>
2025-03-24 15:31:46 -0400

    promise management

--- backend/app/api/routes/app_requests.py
+++ backend/app/api/routes/app_requests.py
@@ -99,7 +99,8 @@ async def get_ready_runner(
                                                 False)
     else:
         # Launch a new runner and wait for it to be ready.
-        fresh_runner : Runner = await launch_runners(db_image.identifier, 1, initiated_by="app_requests_endpoint_no_pool")[0]
+        fresh_runners : list[Runner] = await launch_runners(db_image.identifier, 1, initiated_by="app_requests_endpoint_no_pool")
+        fresh_runner : Runner = fresh_runner[0]
         # Poll up to 60 seconds (12 attempts, every 5 seconds).
         for _ in range(12):
             runner_management.get_runner_by_id(fresh_runner.id)
