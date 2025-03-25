0023f0666660df17409acda34cf02d0813586bce
LiquidPlummer <kyle.plummer@revature.net>
2025-03-24 21:21:13 -0400

    bugfix

--- backend/app/util/constants.py
+++ backend/app/util/constants.py
@@ -1,5 +1,5 @@
 """Single source of truth for env variables."""
 import os
-max_runner_lifetime = os.getenv("MAX_RUNNER_LIFETIME", str(180))
+max_runner_lifetime = int(os.getenv("MAX_RUNNER_LIFETIME", str(180)))
 domain = os.getenv("DOMAIN", "https://devide.revature.com")
 auth_mode = os.getenv("AUTH_MODE", "ON")
