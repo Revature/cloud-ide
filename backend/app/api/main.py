607198e09d39f0acbd9335dc81b7cf6cf1f0b01c
RevvingUpThatHill <theodore.balashov@revature.com>
2025-04-22 15:58:45 -0400

    remove repeat

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -11,11 +11,7 @@ UNSECURE_ROUTES: tuple = (
     f'{API_VERSION}/machine_auth/?$',
     f'{API_VERSION}/runners/\\d+/state/?$',
     f'{API_VERSION}/?$',
-    # Everything behind the proxy loses the api root path from the URI
-    f'{API_VERSION}/?$',
-    f'{API_VERSION}/machine_auth/?$',
     f'{API_VERSION}/registration/email_invite/?$'
-    # Test keeps api root path
     )
 
 RUNNER_ACCESS_ROUTES: tuple = (
