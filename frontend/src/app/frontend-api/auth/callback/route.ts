75ddf280c93c7c13498b1a6a338f201cd1c48e1e
Charles Jester <charles.jester@revature.com>
2025-05-06 17:07:02 -0400

    updated to include the baseURL within the object as a separate property

--- frontend/src/app/frontend-api/auth/callback/route.ts
+++ frontend/src/app/frontend-api/auth/callback/route.ts
@@ -4,4 +4,4 @@ import { handleAuth } from '@workos-inc/authkit-nextjs';
 // The redirect can be customized: `handleAuth({ returnPathname: '/foo' })`
 
 const baseUrl = process.env.NEXT_PUBLIC_DEPLOYMENT_URL || 'localhost:3000'; // Replace with your actual base URL
-export const GET = handleAuth({returnPathname: `https://${baseUrl}/ui/cloud-connectors`});
+export const GET = handleAuth({returnPathname: 'ui/cloud-connectors', baseURL: `https://${baseUrl}`});
