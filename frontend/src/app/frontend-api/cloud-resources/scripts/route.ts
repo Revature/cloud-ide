9bf1d83355d827f22840e4058a8295551e8054a9
Charles Jester <charles.jester@revature.com>
2025-05-07 12:14:51 -0400

    fixed lint issue

--- frontend/src/app/frontend-api/cloud-resources/scripts/route.ts
+++ frontend/src/app/frontend-api/cloud-resources/scripts/route.ts
@@ -4,7 +4,7 @@ import { NextRequest, NextResponse } from 'next/server';
 
 const apiUrl = process.env.BACKEND_API_URL || 'http://localhost:8000';
 
-export async function GET(request: NextRequest) {
+export async function GET() {
   try {
     const response = await fetch(`${apiUrl}/api/v1/scripts/`, {
       method: 'GET',
