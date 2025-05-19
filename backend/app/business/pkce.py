67edb0a213944ae5e7167ce46d32d73d234e5631
Ashoka Shringla <ashokashringla@gmail.com>
2025-05-19 12:51:07 -0400

    added email to get all and get runner endpoints

--- backend/app/business/pkce.py
+++ backend/app/business/pkce.py
@@ -37,7 +37,6 @@ def verify_token_exp(access_token: str):
     # We will want to add other logic here verifying orginization, user, issuer, role, etc
     return True
 
-
 def find_key_set(kid: str):
     """Find a key set in the cache or get from workos."""
     try:
